734adc9cee25084a038911e6f6ae3189a975463f
luowenqi <wilddogqi@gmail.com>
2020-04-03 14:16:56 +0800

    remove dist

--- dist/Student.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Student {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
-exports.Student = Student;
